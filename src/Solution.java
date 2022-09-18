1d4aadac5535071a46b0f9dae55533d0e252a653
Daniil Kuraev <Daniil.Kuraev@waveaccess.global>
2022-09-19 00:14:48 +0300

    solved: 724. Find Pivot Index

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,27 @@
 public class Solution {
 
+    public int pivotIndex(int[] nums) {
+        int sumNums = sum(nums);
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumNums -= nums[i];
+            if (sumNums == currentSum) {
+                return i;
+            } else {
+                currentSum += nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+    private int sum(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result += num;
+        }
+
+        return result;
+    }
 }
