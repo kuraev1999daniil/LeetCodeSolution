414545e9a571560ba3907bfdb41da189aa5a6f38
Daniil Kuraev <Daniil.Kuraev@waveaccess.global>
2022-09-18 23:48:27 +0300

    solved: 1480. Running Sum of 1d Array

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# LeetCode - Problems - Solutions
+# Easy [1480. Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/)
 
-[LeetCode Account](https://leetcode.com/user0555RC/)
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,14 @@
 public class Solution {
 
+    public int[] runningSum(int[] nums) {
+        if (nums.length == 0) return nums;
+
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] + nums[i];
+        }
+
+        return result;
+    }
 }
