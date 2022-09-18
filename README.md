f01b4891edf9dbb1aa6e8c2b9823025b883cce8f
Daniil Kuraev <Daniil.Kuraev@waveaccess.global>
2022-09-18 23:16:56 +0300

    solved: 876. Middle of the Linked List

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# LeetCode - Problems - Solutions
+# Easy [876. Middle of the Linked List](https://leetcode.com/problems/middle-of-the-linked-list/)
 
-[LeetCode Account](https://leetcode.com/user0555RC/)
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,21 @@
 public class Solution {
 
+    public ListNode middleNode(ListNode head) {
+        if (head.next == null) return head;
+
+        boolean hasNextMiddleValue = false;
+        ListNode middleValue = head;
+        while (head != null) {
+            if (hasNextMiddleValue) {
+                middleValue = middleValue.next;
+            }
+
+            hasNextMiddleValue = !hasNextMiddleValue;
+
+            head = head.next;
+        }
+
+        return middleValue;
+    }
 }
+
